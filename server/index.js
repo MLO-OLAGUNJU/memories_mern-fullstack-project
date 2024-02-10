7100165ac8ec1cf89f6fad801a1a2011a61da012
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-10 17:11:06 +0100

    memories CRUD FULLSTACK MERN APP

--- server/index.js
+++ server/index.js
@@ -2,23 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import { mongoose } from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/mydatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(CONNECTION_URL, {})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
