46da959f37b41dc309c12cb9ba7a145cbe54c10a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-10 16:30:21 +0100

    memories CRUD FULLSTACK MERN APP

--- server/index.js
+++ server/index.js
@@ -11,3 +11,13 @@ app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+Mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.error(message));
