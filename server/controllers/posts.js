637d9f19e9c4a12a9c7380a7f2cf1ab594d6ead0
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-10 17:56:54 +0100

    memories CRUD FULLSTACK MERN APP

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,30 @@
-export const getPosts = (req, res) => {
-  res.send("THIS WORKS");
+import express from "express";
+import mongoose from "mongoose";
+
+import PostMessage from "../models/postMessage.js";
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+    console.log(error.message);
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
