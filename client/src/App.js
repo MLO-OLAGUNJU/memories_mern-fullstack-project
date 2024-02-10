f20deda8ec5c2d3614cbb48cd229320757f58de1
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-10 22:36:34 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import {Cotainer, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-
+import React from "react";
+import memories from "./images/memories.png";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <h1>FORM</h1>
+    </>
+  );
+};
+
+export default Form;
