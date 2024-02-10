87c840b83892ba5e6c9c72762bd86e2a96b5dfbf
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 00:20:30 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,6 @@
 import { combineReducers } from "redux";
+import posts from "./posts";
+
+export default combineReducers({
+  posts,
+});
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,10 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return state;
+      break;
+      return state;
+    default:
+      break;
+  }
+};
