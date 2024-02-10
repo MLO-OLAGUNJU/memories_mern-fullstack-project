08d515741dfc1161d316afe89119acdab4eb1467
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-10 16:26:48 +0100

    memories CRUD FULLSTACK MERN APP

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import { Mongoose } from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/?retryWrites=true&w=majority";
