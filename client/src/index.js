2fa7cce3937a18fc9ae124cd0e497b7c2fc2de5c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 15:37:19 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,14 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
