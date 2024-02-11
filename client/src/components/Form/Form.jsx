bd6070cdc41185fc8424fe1e90b6bc8fe6419413
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 16:19:20 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 const Form = () => {
   const classes = useStyles();
 
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    seletedFile: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -17,7 +24,49 @@ const Form = () => {
           noValidate
           className={classes.form}
           onSubmit={handleSubmit}
-        ></form>
+        >
+          <Typography variant="h6">Creating a Memory</Typography>
+          <TextField
+            name="creator"
+            variant="outlined"
+            label="Creator"
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          />
+          <TextField
+            name="title"
+            variant="outlined"
+            label="Creator"
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          />
+          <TextField
+            name="message"
+            variant="outlined"
+            label="Creator"
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          />
+          <TextField
+            name="tags"
+            variant="outlined"
+            label="Creator"
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          />
+        </form>
       </Paper>
     </>
   );
