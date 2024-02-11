b92b38c01d11bd6955b248886e83849a6804e17a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 16:00:21 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,12 +2,9 @@ export default (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
       return action.payload;
-      break;
     case "CREATE":
       return posts;
-      break;
     default:
       return posts;
-      break;
   }
 };
--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/mydatabase?retryWrites=true&w=majority";
