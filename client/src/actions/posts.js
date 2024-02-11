901abc08faeace6ae62d680a8ad53e606c147607
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 15:51:37 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,7 +1,7 @@
 import * as api from "../api";
 
 //Action Creators
-const getPosts = () => async (dispatch) => {
+export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({ type: "FETCH_ALL", payload: data });
--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Post from "./Post/Post";
-import useStyles from './styles'
-
+import useStyles from "./styles";
+import { useSelector } from "react-redux";
 
 const Posts = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <>
