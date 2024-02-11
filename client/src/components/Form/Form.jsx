25eb35514e6eda7a6071cbd28b5e52851e99b235
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-11 16:06:46 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import useStyles from './styles'
-import [TextField, Button, Typography, Paper] from 'material-ui/core'
+import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 const Form = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <>
       <h1>FORM</h1>
