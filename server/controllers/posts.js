8fb01622d1b0da4639cb880998d69c7c2c37c457
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 21:35:32 +0100

    memories CRUD FULLSTACK MERN APP

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,10 +45,11 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
-  await PostMessage.findByIdAndRemove(id);
+  await PostMessage.findOneAndDelete({ _id: id });
 
-  res.json({ message: "Post deleted successfully!" });
+  res.json({ message: "Post deleted successfully." });
 };
