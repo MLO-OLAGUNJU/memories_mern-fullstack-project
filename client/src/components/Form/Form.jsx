956ecbcd64567fa368d00623de4fb90a09966f13
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 14:32:35 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -56,7 +56,7 @@ const Form = ({ currentId, setCurrentId }) => {
           onSubmit={handleSubmit}
         >
           <Typography variant="h6">
-            ${currentId ? "Editing" : "Creating"} a Memory
+            {currentId ? "Editing" : "Creating"} a Memory
           </Typography>
           <TextField
             name="creator"
