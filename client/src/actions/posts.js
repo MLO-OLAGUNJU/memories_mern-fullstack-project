d5069ffcc303f2c531dc79249b3b26a932c3a9ad
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 20:48:43 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -25,6 +25,6 @@ export const updatePost = (id, post) => async (dispatch) => {
     const { data } = await api.updatePost(id, post);
     dispatch({ type: "UPDATE", payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
