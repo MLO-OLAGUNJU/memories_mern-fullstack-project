c66f32605ba999ed4a67beef8f9cbc32a0067d24
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 14:18:59 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,5 +6,3 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
-
-// export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
