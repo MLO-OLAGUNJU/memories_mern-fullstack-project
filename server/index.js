2a6e8077bb4c322bce6020c0dadb00e355768e2c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-13 00:16:26 +0100

    I updated the app

--- server/index.js
+++ server/index.js
@@ -13,12 +13,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://mlo_dev:mlo1234@cluster0.y0yldya.mongodb.net/mydatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, {})
+  .connect(process.env.CONNECTION_URL, {})
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   })
