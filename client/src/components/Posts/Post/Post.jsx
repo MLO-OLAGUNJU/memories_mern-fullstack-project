ea1198ec0cb6aec19d573382f61654cec8cc756e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 14:38:51 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -46,8 +46,11 @@ const Post = ({ post, setCurrentId }) => {
             {post.tags.map((tag) => `#${tag} `)}
           </Typography>
         </div>
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.title}
+        </Typography>
         <CardContent>
-          <Typography className={classes.title} variant="h5" gutterBottom>
+          <Typography variant="h5" gutterBottom>
             {post.message}
           </Typography>
         </CardContent>
