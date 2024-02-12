4b88dcedb60d60cc0bf7544e875ffd88aec893d1
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-12 23:49:14 +0100

    I updated the app

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -10,10 +10,6 @@ export default (posts = [], action) => {
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case LIKE:
-      return posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
     case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
