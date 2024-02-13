e129495c876d1a21de1622e6d6fb1c1ef0f7c26e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-14 00:16:54 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Auth from "./components/Auth/Auth";
 
-const App = () => {
-  return (
+const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </Container>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Auth = () => {
+  const state = null;
+
+  return <div>AUTH</div>;
+};
+
+export default Auth;
