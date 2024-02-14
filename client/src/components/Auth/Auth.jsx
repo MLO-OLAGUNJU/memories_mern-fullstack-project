38b672d46db25bc4640b9f37634792be0ea42715
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-14 13:04:51 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -8,11 +8,20 @@ import {
   Container,
 } from "@material-ui/core";
 import useStyles from "./styles";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Auth = () => {
-  const state = null;
+  const classes = useStyles();
 
-  return <div>AUTH</div>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
