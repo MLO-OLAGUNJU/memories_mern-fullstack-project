3361e05f9cfddc7085168a2c5dc9e8f7f6e87203
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-15 00:59:52 +0100

    memories CRUD FULLSTACK MERN APP

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 export const signin = async (req, res) => {
-  //function
   const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
@@ -34,5 +33,30 @@ export const signin = async (req, res) => {
 };
 
 export const signup = async (req, res) => {
-  //function
+  const { email, password, firstName, lastName, confirmPassword } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(404).json({ message: "User already exists." });
+
+    if (password !== confirmPassword)
+      return res.status(404).json({ message: "Passwords don't match." });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, "test", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+    console.log(error);
+  }
 };
