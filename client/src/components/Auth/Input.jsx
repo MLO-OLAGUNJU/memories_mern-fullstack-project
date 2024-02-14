dbf385c106dca4d1ff632d3db2c9f20b49bc8cde
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-14 13:49:57 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Auth/Input.jsx
+++ client/src/components/Auth/Input.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
 
-const Input = () => {
-  return <div></div>;
+const Input = ({ name, half }) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        inputProps={
+          name === "password" && {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleShowPassword}>
+                  {type === "password" ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }
+        }
+      />
+    </Grid>
+  );
 };
 
 export default Input;
