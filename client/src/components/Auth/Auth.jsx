7c3b188847eb65bc335dbe741655cafe2277c31b
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-14 13:16:03 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Auth = () => {
   const classes = useStyles();
+  const isSignup = false;
 
   return (
     <Container component="main" maxWidth="xs">
@@ -19,6 +20,7 @@ const Auth = () => {
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
       </Paper>
     </Container>
   );
