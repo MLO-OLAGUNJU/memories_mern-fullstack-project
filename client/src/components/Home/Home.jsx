2d5e9459f4e5b9bc910e1f59ba6e76684b921901
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-15 12:07:35 -0400

    memories CRUD FULLSTACK MERN APP

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Form from "../Form/Form";
 import Posts from "../Posts/Posts";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
 import Paginate from "../Pagination";
+import { useNavigate, useLocation } from "react-router-dom";
+import ChipInput from "material-ui-chip-input";
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  navigate = useNavigate();
+  const query = useQuery();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
 
   useEffect(() => {
     dispatch(getPosts());
