403ecfcb6310cd0d0989ccb11e5422eba06b92d9
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-15 10:48:14 -0400

    memories CRUD FULLSTACK MERN APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Navbar from "./components/NavBar/NavBar";
 import Auth from "./components/Auth/Auth";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => (
   <GoogleOAuthProvider clientId="649637207925-uoousgrg8ev0tgcdk614fhfd1phob6rb.apps.googleusercontent.com">
@@ -12,7 +13,10 @@ const App = () => (
       <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/" exact element={() => <Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/auth" exact element={<Auth />} />
         </Routes>
       </Container>
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const PostDetails = () => {
+  console.log("POST DETAILS");
+  return <div></div>;
+};
+
+export default PostDetails;
