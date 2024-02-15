a94a1e3bc014ad501a0a2d13c59fbdbf59f229b4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-15 01:24:39 +0100

    memories CRUD FULLSTACK MERN APP

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,13 @@
-import axios from "axios";
+import axios, { formToJSON } from "axios";
 
-const url = "http://localhost:5000/posts";
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const fetchPosts = () => axios.get("/posts");
+export const createPost = (newPost) => axios.post("/posts", newPost);
+export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
+  axios.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
+
+export const signin = (FormData) => API.post("/users/signin", FormData);
+export const signup = (FormData) => API.post("/users/signup", FormData);
