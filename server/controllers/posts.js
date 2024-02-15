3158cada527893ad36d4637d904c72bc8713d5bb
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-15 04:19:59 +0100

    memories CRUD FULLSTACK MERN APP

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -18,14 +18,17 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
   try {
-    await newPost.save();
-    newPost.save();
-    res.status(201).json(newPost);
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
-    console.log(error.message);
   }
 };
 
