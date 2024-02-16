06d76bf3b493d0fadad9a6afdd57656c26b94f39
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-16 11:24:42 -0400

    memories CRUD FULLSTACK MERN APP

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -129,4 +129,19 @@ export const likePost = async (req, res) => {
   res.status(200).json(updatedPost);
 };
 
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { value } = req.body;
+
+  const post = await postMessage.findById(id);
+
+  post.comments.push(value);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
+
 export default router;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -21,5 +21,6 @@ router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
 
 export default router;
